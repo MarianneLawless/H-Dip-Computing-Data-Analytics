9cd2b220a1cce8943e9cea467e69d84e1cb282e1
Marianne Lawless <36157521+MarianneLawless@users.noreply.github.com>
2018-03-06 00:51:22 +0000

    Add files via upload

---
+++ CollatzConjecture.py
@@ -0,0 +1,22 @@
+# Marianne Lawless
+# https://en.wikipedia.org/wiki/Collatz_conjecture
+
+n = 1 
+
+while n != 1:
+   print("n")
+  if n % 2 ==0:
+   n = n / 2
+   print ("n")
+  elif:
+     n % 2 !=0:
+     n = int (n * 3 + 1) 
+     print("n") 
+
+print ("The final value of n is":, n)
+    
+    
+    print ("n")
+
+
+ 
