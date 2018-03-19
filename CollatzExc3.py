bc66eb55a78a987f2934035032d420e520bcc1b5
lawlessmarianne@gmail.com <ian@gmit.ie>
2018-03-19 22:51:03 +0000

    Starting again and rewrote the program

---
+++ CollatzExc3.py
@@ -0,0 +1,18 @@
+# Marianne Lawless
+# Programming & Scripting Exercise 3
+# Adapted from https://en.wikipedia.org/wiki/Collatz_conjecture
+
+n = 5 # value of n 
+
+while n <= 1: # while n is less than or equal to 1
+    if (n % 2 == 0): # if n is an even number, do the following 
+        n = n / 2 # divide n by 2 
+    else: # otherwise
+        n = n*3 + 1 # mutiply n by 3 and add 1
+        print(n)
+
+n = n + 1
+
+print ("The final value of n is:" , n )  
+
+    
