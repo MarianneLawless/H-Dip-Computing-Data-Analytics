e60a6967c6701312b946b29c20f818d51b2331a9
lawlessmarianne@gmail.com <ian@gmit.ie>
2018-03-20 23:56:59 +0000

    cahnged code

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -5,12 +5,11 @@ i = 1 # starting from 1
 n = 1 # smallest positive number I am looking for
 l = 2520
 
-while i < 21:
-  for i in range (11,21,2):
-    if n % i == 0: #f must be an even number
-       i = i + 1 + l  #add 1 to the number
+while i < 20: # while i is less than or equal 20
+ if n % i == 0: # n must be an even number
+       i = i + 1   # add 1 to the number
 else: # otherwise
-    n = n + 1 + l #add a number to the number I am looking for 
+    n = n + 1 + l # add a number to the number I am looking for 
     i = 1
     l = 2520
 
