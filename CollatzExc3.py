ce78c03f6b7666d6d43d4cdaadd333cc3f0faa76
lawlessmarianne@gmail.com <ian@gmit.ie>
2018-03-20 00:02:10 +0000

    added comment

--- CollatzExc3.py
+++ CollatzExc3.py
@@ -7,11 +7,10 @@ n = 17 # value of n
 while n <= 1: # while n is less than or equal to 1
     if (n % 2 == 0): # if n is an even number, do the following 
         n = n / 2 # divide n by 2 
-        print(n)
     else: # otherwise
         n = n*3 + 1 # mutiply n by 3 and add 1
-        print(n)
-
+    print(n)
+    
 n = n + 1
 
 print ("The final value of n is:" , n )  
