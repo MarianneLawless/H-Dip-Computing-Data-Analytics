60ee2af3af7731f19f9c23dc550e2b0eef79aa5f
lawlessmarianne@gmail.com <ian@gmit.ie>
2018-03-23 23:02:58 +0000

    added comment

---
+++ factorial.py
@@ -0,0 +1,14 @@
+# Marianne Lawless
+# Exercise 6 - Functions
+# Write a Python script containing a function , and call the function factorial()
+
+def factorial(f): # Function defined as factorial (f)
+    n = 1 
+    while f >= 1: # While f is greater than / equl to 1
+        n = n * f # My starting number n multiplied by the number f ..factorial number 
+        f = f - 1 # loops from f to 0, subtracting 1 on every loop
+    return n
+
+print("The factorial of 5 is", factorial(5))
+print("The factorial of 7 is", factorial(7))
+print("The factorial of 10 is", factorial(10))
