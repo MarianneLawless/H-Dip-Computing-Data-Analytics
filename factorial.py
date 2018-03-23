ccec524e0202c90ee287ecc9e35211c226df4bc6
Marianne Lawless <36157521+MarianneLawless@users.noreply.github.com>
2018-03-23 23:27:53 +0000

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,18 +1,18 @@
+
 # Marianne Lawless
 # Exercise 6 - Functions
 # Write a Python script containing a function , and call the function factorial()
+# Adapted from nbviewer.jupyter.org/github/ianmcloughlin/python-fundame
 
-def factorial(f): # Function defined as factorial (f)
-    n = 1 
-    while f >= 1: # While f is greater than / equl to 1
-        n = n * f # My starting number n multiplied by the number f ..factorial number 
-        f = f - 1 # loops from f to 0, subtracting 1 on every loop
-    return n
+def factorial(f):
+    ans = 1 # number I am looking for 
+    for i in range(1, f + 1): # Every time python goes around the loop, it adds 1
+        ans = ans * i # my ans multiplied the new value for i 
+    return ans
 
-print("The factorial of 5 is", factorial(5))
-print("The factorial of 7 is", factorial(7))
-print("The factorial of 10 is", factorial(10))
-
+print(factorial(5))
+print(factorial(7))
+print(factorial(10))
 The factorial of 5 is 120
 The factorial of 7 is 5040
 The factorial of 10 is 3628800
